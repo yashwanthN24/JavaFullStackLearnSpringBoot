cbcf896d147bb17e3baede66d45d56f75a82d30c
yashwanth <yashwanthnaddana2002@gmail.com>
2026-01-23 10:47:43 +0530

    disable session in springboot and csrf as we will manage authentication and authroization via JWT  thats the industry standard

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,7 +26,10 @@ public class WebSecurityConfig {
                           .requestMatchers("/posts/**").hasAnyRole("ADMIN")// protected route but only user with Role ADMIN can access these routes
                           .anyRequest()
                           .authenticated())
-                  .formLogin(Customizer.withDefaults())
+                  .csrf(csrfConfig -> csrfConfig.disable()) // to disable csrf these two session and csrf we wont woory as we will use JWT authentication thats thne industry standard
+                  .sessionManagement(sessionConfig ->
+                          sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // to remove session
+//                  .formLogin(Customizer.withDefaults())
                   .build();
     }
 

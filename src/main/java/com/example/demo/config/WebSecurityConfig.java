b90ce550a1bd741d1c8681a2ab671ed7cfe2f771
yashwanth <yashwanthnaddana2002@gmail.com>
2026-01-23 10:36:23 +0530

    created inMemory users to test admin role access

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,10 +21,34 @@ public class WebSecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
           return  httpSecurity
                   .authorizeHttpRequests(auth -> auth
-
+                          .requestMatchers("/posts").permitAll() // now posts route is public accessible by all not a protected route
+                          .requestMatchers("/posts/**").hasAnyRole("ADMIN")// protected route but only user with Role ADMIN can access these routes
                           .anyRequest()
                           .authenticated())
                   .formLogin(Customizer.withDefaults())
                   .build();
     }
+
+    @Bean
+    UserDetailsService myInMemoryUserDetailsService(){
+        UserDetails normalUser = User
+                .withUsername("tejas")
+                .password(passwordEncoder().encode("tejas@123"))
+                .roles("USER")
+                .build();
+
+        UserDetails adminUser = User
+                .withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(normalUser , adminUser);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 }
